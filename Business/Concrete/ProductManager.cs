4ea6ffe93dee880c59eb2345a2b23a559f12c393
maliyalcin08 <maliyalcin08@gmail.com>
2021-03-02 00:01:59 +0300

    WebAPI eklendi ve bazı işlemler AOP ile test edildi.

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -39,7 +39,7 @@ namespace Business.Concrete
         public IDataResult<List<Product>> GetAll()
         {
             //iş kodları
-            if (DateTime.Now.Hour==22)
+            if (DateTime.Now.Hour == 3)
             {
                 return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
             }
